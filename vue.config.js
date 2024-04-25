00cf2d14818ce4f409b2d349106981feed83f7a3
Jordi Barea <jordi.barea@deister.es>
2024-04-25 07:04:21 +0200

    setting deploy

--- vue.config.js
+++ vue.config.js
@@ -1,33 +1,7 @@
-module.exports = {
-    // Base URL for your application.
-    // If your application is hosted on a subpath, you'll need to specify that subpath here.
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
     publicPath: process.env.NODE_ENV === 'production'
         ? '/tfm-web/' // Replace 'your-repo-name' with your GitHub repository name
         : '/',
-
-    // Output directory for built files (set to `dist` by default)
-    outputDir: 'dist',
-
-    // Directory where the built files will be placed in production builds
-    assetsDir: 'assets',
-
-    // Whether to use the build of Vue provided by Vue CLI service.
-    runtimeCompiler: false,
-
-    // Whether to generate source maps for production builds.
-    productionSourceMap: true,
-
-    // Configure webpack-dev-server behavior
-    devServer: {
-        // Proxy API requests to another backend
-        proxy: {
-            '/api': {
-                target: 'http://localhost:3000',
-                changeOrigin: true,
-                pathRewrite: {
-                    '^/api': ''
-                }
-            }
-        }
-    }
-}
+    transpileDependencies: true
+})
