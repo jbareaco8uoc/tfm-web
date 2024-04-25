37965d3b9c858df05fe091b6de6221766c440737
Jordi Barea <jordi.barea@deister.es>
2024-04-25 07:15:55 +0200

    setting deploy

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/tfm-web/' // Replace 'your-repo-name' with your GitHub repository name
-        : '/',
+    publicPath: process.env.NODE_ENV === 'production' ? '/tfm-web/' : '/',
     transpileDependencies: true
 })
